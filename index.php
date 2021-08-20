d872d10e076a9ffcfa3faf5c24c710e64062e311
BenjamimEterino <64314311+BenjamimEterino@users.noreply.github.com>
2021-08-20 11:11:36 +0200

    Commit de 20/08

--- index.php
+++ index.php
@@ -31,40 +31,7 @@
 
     
 
-        <div class="about-us small-11 large-12 columns no-padding small-centered">
-
-            <div class="global-page-container">
-                <div id="about-us" class="about-us-title small-12 columns no-padding">
-                <h3>Sobre Nós</h3>
-                <hr></hr>
-                </div>
-               
-                    <img src="img/fachada.jpg" alt="fachada do restaurante">
-                
-
-                <div class="about-us-text">
-                <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus leo mi, 
-                        condimentum ac convallis non, porta ac nibh. Morbi volutpat nibh lectus, quis 
-                        convallis nunc rutrum vitae. Aenean volutpat aliquam elementum. Nunc consequat 
-                        elit odio, vitae suscipit nunc pretium eu. Aenean vitae lacus auctor, condimentum 
-                        ipsum at, suscipit erat. Donec dapibus ullamcorper bibendum. Vestibulum posuere 
-                        augue in lectus dictum tincidunt. Pellentesque ornare eget enim sed dignissim. 
-                        Sed nec nisi suscipit, feugiat risus ac, lacinia elit.
-                    </p>
-                    
-                    <p>
-                        Duis fermentum leo enim, eget dignissim dolor imperdiet at. Sed ut rutrum lacus. 
-                        Aenean eleifend, urna eu dapibus imperdiet, turpis diam tristique mauris, nec 
-                        luctus ante massa eu arcu. Duis tempor risus quis tellus posuere eleifend. 
-                        Donec fringilla nulla ac odio sagittis tincidunt. Phasellus tempus id felis et 
-                        finibus. Aenean felis ligula, varius nec varius at, feugiat nec felis. Morbi 
-                        blandit sapien vel justo consequat laoreet.</p>
-                </div>
-            
-            </div>
-
-        </div>
+      
 
     
         <div class="cardapio small-11 large-12 columns no-padding small-centered">
